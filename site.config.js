cd84fa7eab5ec35efbe4c4c06fb89a849b217ad7
anhnhantn97 <89826873+anhnhantn97@users.noreply.github.com>
2023-02-12 17:17:27 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     image: '/avatar.svg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: 'frontend developer',
     bio: 'I develop everything using node.',
-    email: 'morethanmin.dev@gmail.com',
+    email: 'anhnhantn97pay@gmail.com',
     linkedin: 'morethanmin',
     github: 'morethanmin',
     instagram: '',
@@ -24,7 +24,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: 'https://morethan-log.vercel.app',
+  link: 'https://kanki.me',
   since: 2022, // If leave this empty, current year will be used.
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
