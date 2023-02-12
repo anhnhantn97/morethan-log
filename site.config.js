68a6c4dd64636d9fe339e3f59a3ee19a1ca3491b
anhnhantn97 <89826873+anhnhantn97@users.noreply.github.com>
2023-02-12 17:40:20 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -34,7 +34,7 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId: process.env.NOTION_PAGE_ID,
+    pageId: process.env.aafe17604f3b4a9ba74d51ebe6378422,
   },
 
   // plugin configuration (optional)
